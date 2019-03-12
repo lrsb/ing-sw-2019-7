48fcf94c5b37a2131263cb9270de8e53a560fc37
Lorenzo Siega <github@lorenzosiega.com>
2019-03-12 16:22:37 +0100

    travis

--- src/main/it/polimi/swingproject/Main.java
+++ src/main/it/polimi/swingproject/Main.java
@@ -1,8 +1,7 @@
 package it.polimi.swingproject;
 
 public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
