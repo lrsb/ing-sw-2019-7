e8eb6e6c5617a4a2b0442079c3247a462b49bc84
Goetrie <10466619@polimi.it>
2019-03-25 20:53:55 +0100

    incominciati models

--- src/main/java/it/polimi/ingsw/models/game/Player.java
+++ src/main/java/it/polimi/ingsw/models/game/Player.java
@@ -7,8 +7,9 @@ import java.util.ArrayList;
 
 public class Player {
     private String name;
-    private int hits;//array di giocatori
+    private String[12] hitsTaken;//array di giocatori
     private int deaths;
+    private int points;
     private int[] cubes = {0, 0, 0}; //r, y, b
     private ArrayList<PowerUp> powerups = new ArrayList<>();//3 al massimo e consumabili
     private ArrayList<Weapon> weapons = new ArrayList<>();//3 al massimo e scambiabili
