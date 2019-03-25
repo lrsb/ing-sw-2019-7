91e43c7d9cd205da0b5f615a7a128c3f9a51d498
Goetrie <10466619@polimi.it>
2019-03-25 20:34:12 +0100

    incominciati models

--- src/main/java/it/polimi/ingsw/models/weapons/Weapon.java
+++ src/main/java/it/polimi/ingsw/models/weapons/Weapon.java
@@ -5,14 +5,15 @@ import org.jetbrains.annotations.NotNull;
 
 public class Weapon implements Card {
     private Type type;
-    private AmmoConsumption basicAmmoConsumption;
+    private ColorRDY color; //serve perchè un cubo "color" si aggiunge al costo per "pickUp" al fine di caricare l'arma
+    private AmmoConsumption pickUpAmmoConsumption;  //esiste un costo per tirarla su, ma non uno per la mod basic
     private AmmoConsumption firstOptionalAmmoConsumption;
     private AmmoConsumption secondOptionalAmmoConsumption;
     private AmmoConsumption alternateAmmoConsumption;
 
-    public Weapon(Type type, @NotNull AmmoConsumption basicAmmoConsumption, AmmoConsumption firstOptionalAmmoConsumption, AmmoConsumption secondOptionalAmmoConsumption, AmmoConsumption alternateAmmoConsumption) {
+    public Weapon(Type type, @NotNull AmmoConsumption pickUpAmmoConsumption, AmmoConsumption firstOptionalAmmoConsumption, AmmoConsumption secondOptionalAmmoConsumption, AmmoConsumption alternateAmmoConsumption) {
         this.type = type;
-        this.basicAmmoConsumption = basicAmmoConsumption;
+        this.pickUpAmmoConsumption = pickUpAmmoConsumption;
         this.firstOptionalAmmoConsumption = firstOptionalAmmoConsumption;
         this.secondOptionalAmmoConsumption = secondOptionalAmmoConsumption;
         this.alternateAmmoConsumption = alternateAmmoConsumption;
@@ -22,8 +23,8 @@ public class Weapon implements Card {
         return type;
     }
 
-    public AmmoConsumption getBasicAmmoConsumption() {
-        return basicAmmoConsumption;
+    public AmmoConsumption pickUpAmmoConsumption() {
+        return pickUpAmmoConsumption;
     }
 
     public AmmoConsumption getFirstOptionalAmmoConsumption() {
@@ -43,5 +44,9 @@ public class Weapon implements Card {
         LOCK_RIFLE, MACHINE_GUN, THOR, PLASMA_GUN, WHISPER, ELECTROSCYTHE, TRACTOR_BEAM,
         VORTEX_CANNON, FURNACE, HEATSEEKER, HELLION, FLAMETHROWER, GRENADE_LAUNCHER, ROCKET_LAUNCHER,
         RAILGUN, CYBERBLADE, ZX2, SHOTGUN, POWER_GLOVE, SHOCKWAVE, SLEDGEHAMMER
+    }
+
+    public enum ColorRDY {
+        BLUE, RED, YELLOW
     }
 }
