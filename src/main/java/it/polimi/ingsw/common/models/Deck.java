131cac2f9efe0f5bd6b82d4c1912762e2cdbaeeb
SaltyThePolo <mattia.riviera@mail.polimi.it>
2019-04-09 16:33:00 +0200

    preparing for Deck javadoc

--- src/main/java/it/polimi/ingsw/common/models/Deck.java
+++ src/main/java/it/polimi/ingsw/common/models/Deck.java
@@ -8,6 +8,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * This class is composed of three deck that contains the playable, exited ( on the game board or in players' hand)
+ * and discarded (already used and need to be reshuffled) cards.
+ * @param <T> Indicate the type of cards in the deck.
+ */
 public class Deck<T> {
     private @NotNull ArrayList<T> playableCards = new ArrayList<>();
     private @NotNull ArrayList<T> exitedCards = new ArrayList<>();
@@ -19,6 +24,11 @@ public class Deck<T> {
         this.shufflable = shufflable;
     }
 
+    /**
+     * This method move a card from the playable deck to the discarded one.
+     * @return The discarded card.
+     * @throws EmptyDeckException Thrown when there are no more available cards, and the deck is not shufflable.
+     */
     public @NotNull T discardCard() throws EmptyDeckException {
         if (playableCards.isEmpty()) shuffleDeck();
         var discardedCard = playableCards.remove(0);
@@ -26,6 +36,12 @@ public class Deck<T> {
         return discardedCard;
     }
 
+    /**
+     * This method is used to remove a card from playable deck and
+     * (ex: replacement of card that are on the ground)
+     * @return Name of exited card
+     * @throws EmptyDeckException Thrown when there are no more available cards, and the deck is not shufflable.
+     */
     public @NotNull T exitCard() throws EmptyDeckException {
         if (playableCards.isEmpty()) shuffleDeck();
         var exitedCard = playableCards.remove(0);
@@ -33,6 +49,12 @@ public class Deck<T> {
         return exitedCard;
     }
 
+    /**
+     *This method is used when more cards are removed from playable card deck adding them to the exited one
+     * @param n Number of card that you have to remove from the deck.
+     * @return List of card removed from the deck.
+     * @throws EmptyDeckException Thrown when there are no more available cards, and the deck is not shufflable.
+     */
     public @NotNull List<T> exitCards(int n) throws EmptyDeckException {
         if (playableCards.isEmpty()) shuffleDeck();
         var list = new ArrayList<T>();
@@ -44,6 +66,10 @@ public class Deck<T> {
         return list;
     }
 
+    /**This method is used when an exited card is used, it will be added to discarded card deck
+     *
+     * @param exitedCard Name of the card that has been used.
+     */
     public void discardCard(@NotNull T exitedCard) {
         if (exitedCards.indexOf(exitedCard) >= 0) exitedCards.remove(exitedCard);
         discardedCards.add(exitedCard);
