f2d50748cfff17e23f52d72538b60424af4cfcab
SaltyThePolo <mattia.riviera@mail.polimi.it>
2019-05-23 16:41:15 +0200

    CLI up to game selection

--- src/main/java/it/polimi/ingsw/client/views/cli/Cli.java
+++ src/main/java/it/polimi/ingsw/client/views/cli/Cli.java
@@ -6,17 +6,32 @@ import it.polimi.ingsw.client.network.ClientRestImpl;
 import it.polimi.ingsw.client.network.ClientRmiImpl;
 import it.polimi.ingsw.client.network.ClientSocketImpl;
 import it.polimi.ingsw.client.others.Preferences;
+import it.polimi.ingsw.common.models.Room;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.io.PrintStream;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.util.List;
 import java.util.Scanner;
 
 public class Cli {
     private static@NotNull Scanner in = new Scanner(System.in);
+
+    public final static void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (final Exception e) {
+            //  Handle any exceptions.
+        }
+    }
 
     private static void connType() throws RemoteException {
         System.out.println("Ciao,benvenuto in Adrenalina, come vuoi effettuare la connessione?");
@@ -53,7 +68,11 @@ public class Cli {
                 }
                 break;
         }
-        if (Preferences.isLoggedIn()) mainmenu();
+        if (Preferences.isLoggedIn()) {
+            clearConsole();
+            mainmenu();
+        }
+        clearConsole();
         login();
     }
 
@@ -66,8 +85,10 @@ public class Cli {
             var token = Client.API.authUser(nickname, psw);
             if (token == null) {
                 System.out.println("Nickname e/o password errate!!");
+                login();
             } else {
                 Preferences.setToken(token);
+                clearConsole();
                 mainmenu();
             }
         } catch (RemoteException ex) {
@@ -77,7 +98,67 @@ public class Cli {
     }
 
     private static void mainmenu(){
+        System.out.println("Adrenalina");
+        System.out.println(" ");
+        System.out.println("1: nuova partita");
+        System.out.println("2: elenco partite");
+        @NotNull var input = in.nextInt();
+        switch (input){
+            case 1:
+                clearConsole();
+                newGame();
+                break;
+            case 2:
+                clearConsole();
+                joinGame();
+                break;
+        }
+    }
+    private static void newGame(){
+        //TODO
+    }
 
+    private static List<Room> update() {
+            try {
+                var roomList = Client.API.getRooms(Preferences.getToken());
+                if (roomList == null) {
+                    System.out.println("Problemi col server!!");
+                    return null;
+                }
+                var rooms = roomList;
+                return rooms;
+            } catch (RemoteException ex) {
+                ex.printStackTrace();
+                System.out.println("Problemi col server!!");
+            }
+        return null;
+    }
+
+    private static void joinGame(){
+    var rooms = update();
+        System.out.println("_____________________________________________________________________________");
+        System.out.println("");
+        System.out.printf("%10s %15s %30s","PARTITA", "N. GIOCATORI", "GIOCATORI NELLA LOBBY");
+        System.out.println();
+        System.out.println("_____________________________________________________________________________");
+        System.out.println("");
+        for(var room: rooms ){
+            System.out.printf("%30s %30s %30s",room.getName(), room.getUsers().size() + "/5", getUsersList(room));
+            System.out.println();
+        }
+
+    }
+
+    private static String getUsersList(Room rooms){
+        String playerList = "(";
+        for (int i=0; i<rooms.getUsers().size();i++){
+            playerList =playerList + rooms.getUsers().get(i).getNickname();
+            if(i != (rooms.getUsers().size()-1)){
+                playerList = playerList + ", ";
+            }
+        }
+        playerList = playerList + ")";
+        return playerList;
     }
     public static void start() throws RemoteException {
         connType();
