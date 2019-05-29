93a3a767af80d43ceb9021b4a8dacf8a0745728c
lrsb <github@lorenzosiega.com>
2019-05-29 22:59:08 +0200

    follettina in cli

--- src/main/java/it/polimi/ingsw/Client.java
+++ src/main/java/it/polimi/ingsw/Client.java
@@ -22,7 +22,7 @@ public class Client {
                     new NavigationController(LoginViewController.class);
                     return;
                 case "-c":
-                    CliMenuManager.startCli(StartupCli.class);
+                    CliMenuManager.startCli(StartupCli.class, true);
                     return;
             }
         //Client.API = new ClientRestImpl("ing-sw-2019-7.herokuapp.com");
--- src/main/java/it/polimi/ingsw/client/views/cli/base/CliMenuManager.java
+++ src/main/java/it/polimi/ingsw/client/views/cli/base/CliMenuManager.java
@@ -1,14 +1,31 @@
 package it.polimi.ingsw.client.views.cli.base;
 
+import it.polimi.ingsw.client.controllers.pregame.RoomViewController;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class CliMenuManager {
-    public static <T> void startCli(@NotNull Class<T> aClass) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public static <T> void startCli(@NotNull Class<T> aClass, boolean withFollettina) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        @Nullable Clip clip = null;
+        if (withFollettina) try {
+            clip = AudioSystem.getClip();
+            var audioInputStream = AudioSystem.getAudioInputStream(RoomViewController.class.getResource(RoomViewController.class.getSimpleName() + "/follettina.wav"));
+            clip.open(audioInputStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            clip.start();
+        } catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) {
+            e.printStackTrace();
+        }
         invoke(aClass.getMethod("start"), null);
+        if (clip != null) clip.stop();
     }
 
     private static void invoke(@NotNull Method method, @Nullable Object object) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
