5f4262d5037f7c897b5fc2d2b62b3de7c8e0da81
lrsb <github@lorenzosiega.com>
2019-06-04 14:26:04 +0200

    cli manager bug  fix

--- src/main/java/it/polimi/ingsw/Client.java
+++ src/main/java/it/polimi/ingsw/Client.java
@@ -8,13 +8,13 @@ import it.polimi.ingsw.client.views.cli.StartupCli;
 import it.polimi.ingsw.client.views.cli.base.CliMenuManager;
 import it.polimi.ingsw.common.network.API;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.rmi.RemoteException;
 
 public class Client {
     public static API API;
 
-    public static void main(String[] args) throws RemoteException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+    public static void main(String[] args) throws IOException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InterruptedException {
         if (args != null) for (var arg : args)
             switch (arg) {
                 case "-h":
