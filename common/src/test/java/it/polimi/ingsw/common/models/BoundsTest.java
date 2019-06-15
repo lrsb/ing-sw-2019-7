b57a290edf22038c08006d2f94532d339675b998
Goetrie <10466619@polimi.it>
2019-06-15 18:27:48 +0200

    BoundsTest Class : created

---
+++ common/src/test/java/it/polimi/ingsw/common/models/BoundsTest.java
@@ -0,0 +1,43 @@
+package it.polimi.ingsw.common.models;
+
+import org.junit.jupiter.api.RepeatedTest;
+
+import java.security.SecureRandom;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BoundsTest {
+
+    @RepeatedTest(value = 100)
+    void testBounds() {
+        SecureRandom rand = new SecureRandom();
+        Bounds bounds = new Bounds(Bounds.Type.values()[rand.nextInt(Bounds.Type.values().length)],
+                Bounds.Type.values()[rand.nextInt(Bounds.Type.values().length)],
+                Bounds.Type.values()[rand.nextInt(Bounds.Type.values().length)],
+                Bounds.Type.values()[rand.nextInt(Bounds.Type.values().length)]);
+        for (Bounds.Direction dir : Bounds.Direction.values()) {
+            assertNotNull(bounds.getType(dir));
+            bounds.setType(dir, Bounds.Type.values()[rand.nextInt(Bounds.Type.values().length)]);
+            assertNotNull(bounds.getType(dir));
+            switch (dir) {
+                case N:
+                    assertEquals(0, dir.getdX());
+                    assertEquals(1, dir.getdY());
+                    break;
+                case E:
+                    assertEquals(1, dir.getdX());
+                    assertEquals(0, dir.getdY());
+                    break;
+                case S:
+                    assertEquals(0, dir.getdX());
+                    assertEquals(-1, dir.getdY());
+                    break;
+                case W:
+                    assertEquals(-1, dir.getdX());
+                    assertEquals(0, dir.getdY());
+                    break;
+            }
+        }
+
+    }
+}
