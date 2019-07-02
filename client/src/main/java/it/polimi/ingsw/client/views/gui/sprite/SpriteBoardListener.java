9e47c9018fd05434583f5357568e0c88ba2f5cfc
mattia <tyarivi@gmail.com>
2019-07-02 04:45:59 +0200

    Player's weapon added to playerboard, animation on mouseover added, siamo molto fighi

--- client/src/main/java/it/polimi/ingsw/client/views/gui/sprite/SpriteBoardListener.java
+++ client/src/main/java/it/polimi/ingsw/client/views/gui/sprite/SpriteBoardListener.java
@@ -2,8 +2,12 @@ package it.polimi.ingsw.client.views.gui.sprite;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public interface SpriteBoardListener {
     void onSpriteClicked(@NotNull Sprite sprite);
 
     void onSpriteDragged(@NotNull Sprite sprite);
+
+    void onSpriteHovered(@NotNull List<Sprite> sprites);
 }
